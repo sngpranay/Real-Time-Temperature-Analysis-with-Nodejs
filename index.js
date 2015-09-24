6c046ff6d9211e45dc88e4eaf491239bc4438d77
Pranay Singh <sng.pranay@gmail.com>
2015-09-24 17:42:43 -0400

    updated to handle four sensors

--- index.js
+++ index.js
@@ -13,16 +13,45 @@ var sp;
 
 sp = new SerialPort.SerialPort(portName, portConfig);
 
-app.get('/', function(req, res){
-  res.sendfile('index.html');
+app.get('/rt', function(req, res){
+  res.sendfile('realTime.html');
 });
+
+app.get('/', function(req,res){
+	res.sendfile('index.html');
+});
+
+
 
 http.listen(3000, function(){
   console.log('listening on *:3000');
 });
 
-  sp.on('data', function(data) {
-    console.log('data received: ' + data);
-    io.emit("chat message", data);
-  });
-// });
+
+
+
+
+	sp.on('data', function(data) {
+    console.log(data);
+			message = data.split(',');
+			// console.log(message[0]);
+			if (message[0] === 'A'){
+				data = message[1];
+				console.log(data);
+				io.emit("A",data);
+			}
+			if (message[0] === 'B'){
+				data = message[1];
+				io.emit("B", data);
+			}
+			if (message[0] === 'C'){
+				data = message[1];
+				io.emit("C", data);
+			}
+			if (message[0] === 'D'){
+				data = message[1];
+				io.emit("D", data);
+			}
+
+
+});
