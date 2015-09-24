b3e36e96cf6797f7e1186df2bba0a7a6029d48d2
Pranay Singh <sng.pranay@gmail.com>
2015-09-23 23:26:41 -0400

    First commit

---
+++ index.js
@@ -0,0 +1,28 @@
+var SerialPort = require("serialport");
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+var portName = process.argv[2],
+portConfig = {
+	baudRate: 9600,
+	parser: SerialPort.parsers.readline("\n")
+};
+
+var sp;
+
+sp = new SerialPort.SerialPort(portName, portConfig);
+
+app.get('/', function(req, res){
+  res.sendfile('index.html');
+});
+
+http.listen(3000, function(){
+  console.log('listening on *:3000');
+});
+
+  sp.on('data', function(data) {
+    console.log('data received: ' + data);
+    io.emit("chat message", data);
+  });
+// });
